36501d8a2c4d74186ba2d0affb4ddf43c7e1c047
GUPTA-AMBAR <ambargupta1290@gamil.com>
2024-12-22 16:03:48 +0530

    cors_socket_corrected

--- socket.io/socket.js
+++ socket.io/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         // origin: ["http://localhost:5000"],
-        origin: ["https://chat-frontend-7epp.vercel.app"],
+        origin: ["https://chat-frontend-ad9z.vercel.app"],
         methods: ["GET", "POST"],
         credentials: true,
     },
